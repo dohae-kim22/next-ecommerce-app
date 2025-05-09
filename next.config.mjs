fc4cbe6dc98d62db41616915f7563c8e3dd6d9f6
DODO <dohae.kim22@gmail.com>
2025-05-09 21:37:50 +0200

    Chore: Update image domain config to use remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["developers.google.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "developers.google.com",
+        pathname: "/identity/images/**",
+      },
+    ],
   },
 };
 
