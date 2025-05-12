7261eaee4d916ab09cfadcc5198cf49aa942ceec
DODO <dohae.kim22@gmail.com>
2025-05-12 19:35:46 +0200

    Feat: Add price range calculation to product slice

--- src/redux/slice/productSlice.js
+++ src/redux/slice/productSlice.js
@@ -4,14 +4,32 @@ const productSlice = createSlice({
   name: "product",
   initialState: {
     products: [],
+    minPrice: null,
+    maxPrice: null,
   },
   reducers: {
     setProducts: (state, action) => {
       state.products = action.payload.products;
     },
+    getPriceRange: (state, action) => {
+      const { products } = action.payload;
+      const arr = [];
+      products.map((product) => {
+        const price = product.price;
+        return arr.push(price);
+      });
+
+      const max = Math.max(...arr);
+      const min = Math.min(...arr);
+      state.minPrice = min;
+      state.maxPrice = max;
+    },
   },
 });
 
-export const { setProducts, setSelectedProduct } = productSlice.actions;
+export const { setProducts, getPriceRange } = productSlice.actions;
 export const selectProducts = (state) => state.product.products;
+export const selectMinPrice = (state) => state.product.minPrice;
+export const selectMaxPrice = (state) => state.product.maxPrice;
+
 export default productSlice.reducer;
