f4f4e54db3b7cf748e8aac7b2bc81aed76e77ac6
DODO <dohae.kim22@gmail.com>
2025-05-13 00:32:02 +0200

    Feat: Combine category, brand and price filtering into single reducer

--- src/redux/slice/filterSlice.js
+++ src/redux/slice/filterSlice.js
@@ -34,10 +34,31 @@ const filterSlice = createSlice({
       tempProducts = products.filter((product) => product.price <= price);
       state.filteredProducts = tempProducts;
     },
+    filterBy: (state, action) => {
+      const { products, category, brand, price } = action.payload;
+      let tempProducts = [];
+
+      if (category === "All") {
+        tempProducts = products;
+      } else {
+        tempProducts = products.filter(
+          (product) => product.category === category
+        );
+      }
+      if (brand === "All") {
+        tempProducts = tempProducts;
+      } else {
+        tempProducts = tempProducts.filter(
+          (product) => product.brand === brand
+        );
+      }
+      tempProducts = tempProducts.filter((product) => product.price <= price);
+      state.filteredProducts = tempProducts;
+    },
   },
 });
 
-export const { filterByCategory, filterByBrand, filterByPrice } =
+export const { filterByCategory, filterByBrand, filterByPrice, filterBy } =
   filterSlice.actions;
 
 export const selectFilteredProducts = (state) => state.filter.filteredProducts;
