8b02b773f488801acd1001cdc704ec57073679cc
DODO <dohae.kim22@gmail.com>
2025-05-14 01:53:12 +0200

    Feat: Add inline loader

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import styles from "./Loader.module.scss";
 import { RotatingLines } from "react-loader-spinner";
 
-const Loader = () => {
+const Loader = ({ inline }) => {
+  if (inline) {
+    return (
+      <div className={styles.inlineWrapper}>
+        <RotatingLines
+          visible={true}
+          width="40"
+          strokeWidth="5"
+          strokeColor="grey"
+          animationDuration="0.75"
+          ariaLabel="rotating-lines-loading"
+        />
+      </div>
+    );
+  }
   return (
     <div className={styles.loaderWrapper}>
       <div className={styles.loader}>
