06273121671a3fe68e6e379dfb48af247e9095b0
DODO <dohae.kim22@gmail.com>
2025-05-14 16:17:49 +0200

    Feat: Add Product Component

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -3,8 +3,17 @@
 import React, { useEffect } from "react";
 import useFetchCollection from "@/hooks/useFetchCollection";
 import styles from "./Product.module.scss";
-import { selectProducts, setProducts } from "@/redux/slice/productSlice";
+import {
+  getPriceRange,
+  selectMaxPrice,
+  selectMinPrice,
+  selectProducts,
+  setProducts,
+} from "@/redux/slice/productSlice";
 import { useDispatch, useSelector } from "react-redux";
+import ProductFilter from "./productFilter/ProductFilter";
+import Loader from "../loader/Loader";
+import ProductList from "./productList/ProductList";
 
 const Product = () => {
   const { data, isLoading } = useFetchCollection("products");
@@ -13,14 +22,22 @@ const Product = () => {
 
   useEffect(() => {
     dispatch(setProducts({ products: data }));
+
+    dispatch(getPriceRange({ products: data }));
   }, [data, dispatch]);
 
   const products = useSelector(selectProducts);
+  const minPrice = useSelector(selectMinPrice);
+  const maxPrice = useSelector(selectMaxPrice);
 
   return (
     <section className={styles.product}>
-      <aside className={styles.filter}></aside>
-      <div className={styles.content}></div>
+      <aside className={styles.filter}>
+        {isLoading ? null : <ProductFilter />}
+      </aside>
+      <div className={styles.content}>
+        {isLoading ? <Loader inline /> : <ProductList />}
+      </div>
     </section>
   );
 };
