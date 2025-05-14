2b4ad5bc7b82019b6ddc597890928070609e5a65
DODO <dohae.kim22@gmail.com>
2025-05-14 16:17:01 +0200

    Feat: Add Product Item Component

---
+++ src/components/product/productItem/ProductItem.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import styles from "./ProductItem.module.scss";
+import { Rating } from "react-simple-star-rating";
+
+const ProductItem = ({ id, name, price, brand, imageUrl }) => {
+  const shortenTitle = (title, n) => {
+    const words = title.split(" ");
+    return words.length > n ? words.slice(0, n).join(" ") + "..." : title;
+  };
+
+  shortenTitle(name);
+  return (
+    <div className={styles.productItem}>
+      <Link href={`/product-details/${id}`}>
+        <div className={styles.img}>
+          <Image src={imageUrl} alt="name" width={230} height={230} />
+        </div>
+        <div className={styles.productInfo}>
+          <p className={styles.title}>{shortenTitle(name, 10)}</p>
+          <p className={styles.brand}>{brand}</p>
+        </div>
+        <div className={styles.priceSection}>
+          <p className={styles.price}>{price}</p>
+          <span className={styles.unit}>€</span>
+        </div>
+        <div>
+          <Rating readonly size={18} initialValue={0} />
+          <span className={styles.ratingCount}>(3)</span>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default ProductItem;
